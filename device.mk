264106c7ee59e2889758a93e97b053062465facc
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:37:07 +0530

    X00TD: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914

--- device.mk
+++ device.mk
@@ -436,6 +436,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
