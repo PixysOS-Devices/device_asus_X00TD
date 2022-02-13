8e220a5197a41b03e4da36c11b1626c2df3de697
sb6596 <shubhamprince111@gmail.com>
2022-02-13 13:48:06 +0530

    X00TD: Build QTI Thermal HAL
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: If342592c04a16604b45a39750161cc3817358d77

--- device.mk
+++ device.mk
@@ -451,7 +451,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.asus_sdm660.qti
 
 # USB
 PRODUCT_PACKAGES += \
